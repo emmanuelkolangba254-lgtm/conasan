f8c2799a247ffbbd592fc2da65c5cbd2a37f649c
Emma Lionel <emmanuelkolangba254@gmail.com>
2026-08-14 09:47:41 +0100

    Configure Supabase Storage

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'core',
     'accueil',
     'contenu',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -166,3 +167,26 @@ LOCALE_PATHS = [
 BASE_DIR / "locale",
 
 ]
+# ==============================
+# SUPABASE STORAGE
+# ==============================
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = None
+
+MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/"
